201c969638bc0ff19d646fccc331021ce28c93bd
Will Stone <654103+will-stone@users.noreply.github.com>
2021-03-24 09:58:12 +0000

    Update ls.fish

--- functions/ls.fish
+++ functions/ls.fish
@@ -1,3 +1,3 @@
-function ls --wraps ls --description 'List contents of directory'
-  command ls -AFG $argv
+function ls --wraps exa --description 'List contents of directory'
+  command exa --all --group-directories-first --icons $argv
 end
