14b0f40efb7cab4e951cb3be5589be06c98598db
Will Stone <654103+will-stone@users.noreply.github.com>
2021-03-24 10:39:28 +0000

    Update v.fish

--- functions/v.fish
+++ functions/v.fish
@@ -1,21 +1,21 @@
-function _v_semver_version
-  set -l string $argv[1]
-  set -l semver (string match -r "\d+\.\d+\.\d+" $string)
-  echo $semver
-end
-
-function _v_version_entry
-  set -l cmd $argv[1]
-  set -l arg $argv[2]
-
-  if type -q $cmd
-    set -l ver (eval $cmd $arg)
-    set -l semver (_v_semver_version $ver)
-    echo "$cmd|$semver"
+function v --description 'Shows versions for commonly installed items'
+  function _v_semver_version
+    set -l string $argv[1]
+    set -l semver (string match -r "\d+\.\d+\.\d+" $string)
+    echo $semver
   end
-end
 
-function _v_all_versions
+  function _v_version_entry
+    set -l cmd $argv[1]
+    set -l arg $argv[2]
+
+    if type -q $cmd
+      set -l ver (eval $cmd $arg)
+      set -l semver (_v_semver_version $ver)
+      printf "$cmd\t$semver\n"
+    end
+  end
+
   set_color green
   _v_version_entry "node" "-v"
   
@@ -36,8 +36,4 @@ function _v_all_versions
 
   set_color blue
   _v_version_entry "fish" "--version"
-end
-
-function v --description 'Shows versions for commonly installed items'
-  _v_all_versions | column -t -s "|"
 end
